3a89fee4e22097dff42b8377b4ebb075774f30b8
Ntinga <st10486811@vcconnect.edu.za>
2026-03-31 20:59:53 +0200

    Initial Commit, includes playVoiceMessage method

---
+++ CyberSecurity ChatBot POE - ST10486811/CyberSecurity ChatBot POE - ST10486811/ChatBotClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurity_ChatBot_POE___ST10486811
+{
+    internal class ChatBotClass
+    {
+        public string UserName { get; set; }
+
+        public void playVoiceMessage()
+        {
+            try
+            {
+                using (SoundPlayer player = new SoundPlayer(""))
+                {
+                    player.PlaySync();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("System Note: Welcome audio not found.");
+            }
+        }
+
+
+    }
+}
---
+++ CyberSecurity ChatBot POE - ST10486811/CyberSecurity ChatBot POE - ST10486811/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurity_ChatBot_POE___ST10486811
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

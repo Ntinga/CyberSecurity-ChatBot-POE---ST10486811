54b7fd4892d3f4df41f74e4ce60213e019646fb1
Ntinga <st10486811@vcconnect.edu.za>
2026-03-31 21:26:35 +0200

    Added UIClass
    Added UserInterface Class
    Added ASCII art

---
+++ CyberSecurity ChatBot POE - ST10486811/CyberSecurity ChatBot POE - ST10486811/UIClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurity_ChatBot_POE___ST10486811
+{
+    internal class UIClass
+    {
+        public static void displayLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+                            + "@  ____                       _ _         @"
+                            + "@ / ___|  ___  ___ _   _ _ __(_) |_ _   _ @"
+                            + "@ \___  / _ \/ __| | | | '__| | __| | | |@"
+                            + "@  ___) | ___/\(__| |_| | |  | | |_| |_| |@"
+                            + "@ |____/ \___| \__|\__,_|_|  |_|\__|\__, |@"
+                            + "@                                   |___/ @"
+                            + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.WriteLine("Welcome!");
+            Console.ResetColor();
+        }
+    }
+}

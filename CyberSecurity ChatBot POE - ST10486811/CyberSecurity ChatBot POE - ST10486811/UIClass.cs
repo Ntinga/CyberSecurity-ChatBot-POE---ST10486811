60f2602e058627217cef4f2f3d8ca0b2a5c90465
Ntinga <st10486811@vcconnect.edu.za>
2026-03-31 21:46:10 +0200

    Fixing bug

--- CyberSecurity ChatBot POE - ST10486811/CyberSecurity ChatBot POE - ST10486811/UIClass.cs
+++ CyberSecurity ChatBot POE - ST10486811/CyberSecurity ChatBot POE - ST10486811/UIClass.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CyberSecurity_ChatBot_POE___ST10486811
@@ -11,15 +12,27 @@ namespace CyberSecurity_ChatBot_POE___ST10486811
         public static void displayLogo()
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-                            + "@  ____                       _ _         @"
-                            + "@ / ___|  ___  ___ _   _ _ __(_) |_ _   _ @"
-                            + "@ \___  / _ \/ __| | | | '__| | __| | | |@"
-                            + "@  ___) | ___/\(__| |_| | |  | | |_| |_| |@"
-                            + "@ |____/ \___| \__|\__,_|_|  |_|\__|\__, |@"
-                            + "@                                   |___/ @"
-                            + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            Console.WriteLine("@  ____                       _ _         @");
+            Console.WriteLine("@ / ___|  ___  ___ _   _ _ __(_) |_ _   _ @");
+            Console.WriteLine("@ |___   / _ |/ __| | | | '__| | __| | | |@");
+            Console.WriteLine("@  ___) | ___/|(__| |_| | |  | | |_| |_| |@");
+            Console.WriteLine("@ |____/ |___| |__|___,_|_|  |_||__||__, |@");
+            Console.WriteLine("@                                   |___/ @");
+            Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
             Console.WriteLine("Welcome!");
+            Console.ResetColor();
+        }
+
+        public static void typeMessage(string message, ConsoleColor color = ConsoleColor.DarkBlue)
+        {
+            Console.ForegroundColor = color;
+            foreach (char c in message)
+            {
+                Console.Write(c);
+                Thread.Sleep(10);
+            }
+            Console.WriteLine();
             Console.ResetColor();
         }
     }
